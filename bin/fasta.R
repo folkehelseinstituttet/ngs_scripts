531cf2f9ffe4db365f903b3d7409dd0aa5df23a5
jonbra <jon.brate@fhi.no>
2023-02-22 10:25:43 +0100

    Minor fixes

--- bin/fasta.R
+++ bin/fasta.R
@@ -10,7 +10,7 @@ library(readxl)
 
 # Load metadata
 args = commandArgs(trailingOnly=TRUE)
-if (length(args) < 8) {
+if (length(args) < 10) {
     stop("Usage: fasta.R <csv> <FHI_1> <FHI_2> <MIK> <Artic_1> <Artic_2> <Nano_1> <Nano_2>", call.=FALSE)
 }
 
@@ -20,15 +20,17 @@ if (length(args) < 8) {
 #  # Remove rows starting with "#"
 #  filter(str_detect(platform, "^#", negate = TRUE))
 #
-metadata      <- read_csv(args[1]) 
+metadata      <- read_csv(args[1])
 
 FHI_files_1   <- args[2] # FHI_files_1 <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Illumina_NSC_FHI/2021/"
 FHI_files_2   <- args[3] # FHI_files_2 <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Illumina_NSC_FHI/2022/"
-MIK_files     <- args[4] # MIK_files <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Illumina_NSC_MIK"
-Artic_files_1 <- args[5] # Artic_files_1 <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Illumina/2021"
-Artic_files_2 <- args[6] # Artic_files_2 <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Illumina/2022"
-Nano_files_1  <- args[7] # Nano_files_1 <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Nanopore/2021"
-Nano_files_2  <- args[8] # Nano_files_2 <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Nanopore/2022"
+FHI_files_3   <- args[4] # FHI_files_2 <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Illumina_NSC_FHI/2023/"
+MIK_files     <- args[5] # MIK_files <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Illumina_NSC_MIK"
+Artic_files_1 <- args[6] # Artic_files_1 <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Illumina/2021"
+Artic_files_2 <- args[7] # Artic_files_2 <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Illumina/2022"
+Nano_files_1  <- args[8] # Nano_files_1 <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Nanopore/2021"
+Nano_files_2  <- args[9] # Nano_files_2 <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Nanopore/2022"
+Nano_files_3  <- args[10] # Nano_files_2 <- "/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/1-Nanopore/2023"
 
 # Open connection to log file
 log_file <- file(paste0(Sys.Date(), "_fasta_raw.log"), open = "a")
@@ -40,17 +42,17 @@ fastas_final <- tibble(
 )
 
 # Start to list directories first
-dirs_fhi  <- c(list.dirs(FHI_files_1, recursive = FALSE), list.dirs(FHI_files_2, recursive = FALSE))
+dirs_fhi  <- c(list.dirs(FHI_files_1, recursive = FALSE), list.dirs(FHI_files_2, recursive = FALSE), list.dirs(FHI_files_3, recursive = FALSE))
 dirs_mik  <- list.dirs(MIK_files, recursive = FALSE)
 dirs_ill  <- c(list.dirs(Artic_files_1, recursive = FALSE), list.dirs(Artic_files_2, recursive = FALSE))
-dirs_nano <- c(list.dirs(Nano_files_1, recursive = FALSE), list.dirs(Nano_files_2, recursive = FALSE))
+dirs_nano <- c(list.dirs(Nano_files_1, recursive = FALSE), list.dirs(Nano_files_2, recursive = FALSE), list.dirs(Nano_files_3, recursive = FALSE))
 
 # Find sequences on N: and create fasta object ----------------------------
 # First create empty data frame to fill
 suppressWarnings(rm(fastas)) # Remove old objects
 fastas <- data.frame(seq.name = character(),
                      seq.text = character())
-pb = txtProgressBar(min = 0, max = nrow(metadata), initial = 0) 
+pb = txtProgressBar(min = 0, max = nrow(metadata), initial = 0)
 for (i in 1:nrow(metadata)) {
   setTxtProgressBar(pb,i)
   if (metadata$code[i] == "FHI"){
@@ -62,9 +64,11 @@ for (i in 1:nrow(metadata)) {
                             pattern = "ivar\\.consensus\\.masked_Nremoved\\.fa$",
                             full.names = TRUE,
                             recursive = TRUE)
-    #samples <- gsub("_.*", "", basename(filepaths))
-
+    
     fasta <- filepaths[grep(metadata$SEARCH_COLUMN[i], filepaths)]
+    # NB: Change this for the new sequence id format
+    # Skip the dash for NSC samples 
+    # fasta <- filepaths[grep(str_remove(metadata$SEARCH_COLUMN[i], "-"), filepaths)]
 
   } else if (metadata$code[i] == "MIK") {
     # Pick our the relevant oppsett
@@ -75,8 +79,6 @@ for (i in 1:nrow(metadata)) {
                             pattern = "ivar\\.consensus\\.masked_Nremoved\\.fa$",
                             full.names = TRUE,
                             recursive = TRUE)
-
-    #samples <- gsub("_.*","", gsub(".*/","", filepaths))
 
     fasta <- filepaths[grep(metadata$SEARCH_COLUMN[i], filepaths)]
 
@@ -90,17 +92,15 @@ for (i in 1:nrow(metadata)) {
                             full.names = TRUE,
                             recursive = TRUE)
 
-    # Dropper det siste tallet.
-    #samples <- gsub("_.*", "", basename(filepaths))
-    #samples <- str_sub(gsub("Artic", "", gsub("_.*","", gsub(".*/","", filepaths))), start = 1, end = -2)
-
     fasta <- filepaths[grep(metadata$SEARCH_COLUMN[i], filepaths)]
 
   } else if (metadata$code[i] == "Artic_Nano") {
 
     # Pick our the relevant oppsett
     oppsett <- gsub("Nr", "", (gsub("/Nano", "", metadata$SETUP[i])))
-    dir <- dirs_nano[grep(paste0(metadata$SETUP[i]), dirs_nano)]
+    # Activate this for new sequence IDs
+    #oppsett <- str_split(oppsett, "/", simplify = TRUE)[,1]
+    dir <- dirs_nano[grep(oppsett, dirs_nano)]
 
     # List the files
     filepaths <- list.files(path = dir,
@@ -108,6 +108,8 @@ for (i in 1:nrow(metadata)) {
                             full.names = TRUE,
                             recursive = TRUE)
 
+    # change dash to underscore for new sequence ids:
+    #fasta <- filepaths[grep(str_replace(metadata$SEARCH_COLUMN[i], "-", "_"), filepaths)]
     fasta <- filepaths[grep(metadata$SEARCH_COLUMN[i], filepaths)]
   }
   
