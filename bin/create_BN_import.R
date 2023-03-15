ec63a2779696d4ce0e6b3ccaf5b2f9b5ec660bbc
jonbra <jon.brate@fhi.no>
2023-03-15 16:02:23 +0100

    Dealing with version numbers in Key

--- bin/create_BN_import.R
+++ bin/create_BN_import.R
@@ -4,7 +4,7 @@
 library(tidyverse)
 
 # NB: This script is based on the input from the cli3 gisaid uploader
-to_BN_rest <- read_tsv(file = "/home/jonr/Downloads/gisaid_cli3/2023-02-22_submission.log", col_names = FALSE) %>% 
+to_BN_rest <- read_tsv(file = "/home/jonr/Prosjekter/FHI_Gisaid/Gisaid_files/2023-03-15_submission.log", col_names = FALSE) %>% 
   # Remove already existing submissions - deal with these later
   filter(str_detect(X1, "error", negate = TRUE)) %>% 
   # Remove summary text
@@ -13,6 +13,8 @@ to_BN_rest <- read_tsv(file = "/home/jonr/Downloads/gisaid_cli3/2023-02-22_submi
   separate(X1, into = c("tmp1", "tmp2"), sep = ";") %>%
   separate(tmp1, into = c(NA, "tmp4"), sep = "Norway/") %>% 
   separate(tmp4, into = c("Key", "year"), sep = "/") %>% 
+  # If there is a "-" in the Key
+  separate(Key, into = c("Key", "version"), sep = "-") %>% 
   mutate(year = str_sub(year, 3, 4)) %>% 
   mutate(gisaid_epi_isl = str_sub(tmp2, 1, -3)) %>%
   # Remove leading white space
@@ -23,6 +25,8 @@ to_BN_rest <- read_tsv(file = "/home/jonr/Downloads/gisaid_cli3/2023-02-22_submi
   add_column("nr" = 25) %>% 
   # Left pad the Key with zeroes to a total of 5 digits
   mutate("Key" = str_pad(Key, width = 5, side = c("left"), pad = "0")) %>% 
+  # Add the version number back, but with "_"
+  unite("Key", c(Key, version), sep = "-", na.rm = TRUE) %>% 
   unite("tmp", c(nr, year, Key), sep = "") %>% 
   add_column("Platform" = NA) %>% 
   # Select final columns
@@ -35,7 +39,7 @@ write.csv(to_BN_rest,
 
 # Import failed/existing submissions from the cli3 uploader
 
-tmp <- read_tsv(file = "/home/jonr/Downloads/gisaid_cli3/2023-02-22_submission.log", col_names = FALSE) %>% 
+tmp <- read_tsv(file = "/home/jonr/Prosjekter/FHI_Gisaid/Gisaid_files/2023-03-15_submission.log", col_names = FALSE) %>% 
   # Remove already existing submissions - deal with these later
   filter(str_detect(X1, "exists", negate = FALSE)) %>% 
   # Remove summary text
@@ -43,7 +47,9 @@ tmp <- read_tsv(file = "/home/jonr/Downloads/gisaid_cli3/2023-02-22_submission.l
   # Remove much junk text and isolate the Key and EPI_ISL
   separate(X1, into = c("tmp1", NA, "tmp2"), sep = ";") %>%
   separate(tmp1, into = c(NA, "tmp4"), sep = "Norway/") %>% 
-  separate(tmp4, into = c("Key", "year"), sep = "/") %>% 
+  separate(tmp4, into = c("Key", "year"), sep = "/") %>%
+  # If there is a "-" in the Key
+  separate(Key, into = c("Key", "version"), sep = "-") %>% 
   mutate(year = str_sub(year, 3, 4)) %>% 
   mutate(gisaid_epi_isl = str_sub(tmp2, 1, -3)) %>%
   mutate(gisaid_epi_isl = str_extract(tmp2, "EPI_ISL_[0-9]+")) %>% 
@@ -53,14 +59,16 @@ tmp <- read_tsv(file = "/home/jonr/Downloads/gisaid_cli3/2023-02-22_submission.l
   # Re-create BN Key
   add_column("nr" = 25) %>% 
   # Left pad the Key with zeroes to a total of 5 digits
-  mutate("Key" = str_pad(Key, width = 5, side = c("left"), pad = "0")) %>% 
+  mutate("Key" = str_pad(Key, width = 5, side = c("left"), pad = "0")) %>%
+  # Add the version number back, but with "_"
+  unite("Key", c(Key, version), sep = "-", na.rm = TRUE) %>% 
   unite("tmp", c(nr, year, Key), sep = "") %>% 
   add_column("Platform" = NA) %>% 
   # Select final columns
   select("Key" = tmp, gisaid_epi_isl, Platform)
 
 write.csv(tmp, 
-          file = "/home/jonr/2023.03.15_4_BN_batch_import.csv",
+          file = "/home/jonr/2023.03.15_5_BN_batch_import.csv",
           quote = TRUE,
           row.names = FALSE)
 
