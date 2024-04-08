42020d1a0a7cf3deb81b8bf44d250e7f3f027d73
Jon Bråte <jon.brate@fhi.no>
2024-04-08 11:45:30 +0200

    Update collect_genoflu_results.R

--- genoflu/collect_genoflu_results.R
+++ genoflu/collect_genoflu_results.R
@@ -1,1 +1,12 @@
 #!/usr/bin/env Rscript
+
+files <- list.files("fasta/", "tsv$", full.names=T)
+
+data_list <- lapply(files, function(file) {
+  read.delim(file, header = TRUE, stringsAsFactors = FALSE)
+})
+
+combined_data <- do.call(rbind, data_list)
+
+# Step 4: Write the combined data frame to a new TSV file
+write.table(combined_data, file = paste0(Sys.Date(), "_genoflu_combined_data.tsv"), sep = "\t", row.names = FALSE)
