07cafee25c50cba4ad4e122b0366784a90b52611
Jon Bråte <jon.brate@fhi.no>
2024-04-08 11:15:46 +0200

    Update wrapper.sh

--- genoflu/wrapper.sh
+++ genoflu/wrapper.sh
@@ -3,30 +3,26 @@
 # Usage
 wrapper.sh <input fasta>
 
-# First install miniconda if it's not already installed
-if ! command -v conda &> /dev/null
+# Check that GenoFlu is installed and available
+if ! command -v genoflu.py &> /dev/null
 then
-    echo "Conda not installed. Installing. Press "Y" to any prompts"
-    mkdir -p ~/miniconda3
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-    bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-    rm -rf ~/miniconda3/miniconda.sh
+    echo "GenoFlu is not available"
+    exit 1
 fi
 
-# Then install R if not installed
+# Check that Rscript is installed and available
 if ! command -v Rscript &> /dev/null
 then
-    conda install conda-forge::r-base
+    echo "Rscript is not available"
+    exit 1
 fi
 
-# Then install GenoFlu
-conda install GenoFlU -c conda-forge -c bioconda
-
 # Then split the multifasta into individual fastas per virus genome
-Rscript
+Rscript <input multi fasta> fasta
 
 # Then run the genotyping
-for i in fasta/*.fasta
+cd fasta
+for i in *.fasta
 do
 genoflu.py -f $i
 done
