64591e20d3406f8ade17191d5a14f6327cb37a7c
Jon Bråte <jon.brate@fhi.no>
2024-04-08 10:04:50 +0200

    Create split_gisaid_multifasta.R

---
+++ genoflu/split_gisaid_multifasta.R
@@ -0,0 +1,24 @@
+library(seqinr)
+args <-
+
+fasta <- args[1]
+prefix <- args[2]
+
+fasta <- read.fasta(fasta)
+
+# Create a function to extract group names from sequence headers
+get_group_name <- function(header) {
+  # Extract the group based on the pattern "EPI_ISL_"
+  group <- gsub('.*\\|EPI_ISL_([^|]+)\\|.*', '\\1', header)
+  return(group)
+}
+
+# Split sequences into groups based on the pattern "EPI_ISL_"
+grouped_sequences <- split(fasta, sapply(names(fasta), get_group_name))
+
+
+# Write sequences into individual fasta files per group
+for (group_name in names(grouped_sequences)) {
+  tmp <- grouped_sequences[[group_name]][1:8]
+  write.fasta(tmp, names = names(tmp), file = paste0(prefix, "EPI_ISL_", group_name, "_sequences.fasta"))
+}
