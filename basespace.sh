8ee96a9c3a9b1544e41c5775b085ac70b5662c4b
Jon Bråte <jon.brate@fhi.no>
2024-04-11 14:25:18 +0200

    Update basespace.sh

--- basespace.sh
+++ basespace.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
-# TODO:
-# [] Set up the smbclient command. Move to N/Virologi/JonBrate when testing
-# [] Run script as ngs user
-# [] Hva skal være $SMB_HOST? (vi flytter fra VM til N)
-# [] Move files to 3-Sekvenseringsbiblioteker in the end (fix the $STAGING variable)
-
 # Maintained by: Jon Bråte (jon.brate@fhi.no)
-# Version: dev
+# Version: 1.0
+
+# This script downloads fastq files from the BaseSpace server and transfers them to the N-drive.
+
+## Check if necessary software and files are present
 
 # The script requires BaseSpace CLI installed (https://developer.basespace.illumina.com/docs/content/documentation/cli/cli-overview)
 # Check if the bs command is available
-#if ! command -v ./bin/bs &> /dev/null
-#then
-#    echo "BaseSpace CLI could not be found"
-#    exit 1
-#fi
+if ! command -v /home/ngs/bin/bs &> /dev/null
+then
+    echo "BaseSpace CLI could not be found"
+    exit 1
+fi
 
 # There also has to be a BaseSpace credentials file: $HOME/.basespace/default.cfg
 # Check if the file exists
 if ! test -f ~/.basespace/default.cfg; then
   echo "BaseSpace credentials file does not exist."
+  exit 1
+fi
+
+# Check if credential file exists
+if ! test -f ~/.smbcreds; then
+  echo "Credential file for transfer to N does not exist."
   exit 1
 fi
 
@@ -32,24 +36,26 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Set the variables
+## Set up environment
+
 SMB_AUTH=/home/ngs/.smbcreds
 SMB_HOST=//Pos1-fhi-svm01/styrt
-SMB_DIR=NGS/3-Sekvenseringsbiblioteker
+SMB_DIR=NGS/3-Sekvenseringsbiblioteker/2024/Illumina_Run
 Run=$1
 Agens=$2
 
 # Make a fastq directory under home. Download files here
 mkdir -p ~/fastq
 
+echo "Getting the Run ID on the BaseSpace server"
 # List Runs on BaseSpace and get the Run id (third column separated by | and whitespaces)
 id=$(/home/ngs/bin/bs list projects | grep "${Run}" | awk -F '|' '{print $3}' | awk '{$1=$1};1')
 
+echo "Downloading fastq files"
 # Then download the fastq files
 /home/ngs/bin/bs download project -i ${id} --extension=fastq.gz -o fastq/${Run}
 
 # Clean up the folder names
-
 RUN_DIR="$HOME/fastq/${Run}"
 # Find only directories in the current directory. Loop through them and rename
 # mindepth 1 excludes the RUN_DIR directory. maxdepth 1 includes only the sudirectories of RUN_DIR
@@ -64,7 +70,8 @@ find "$RUN_DIR" -mindepth 1 -maxdepth 1 -type d -print0 | while IFS= read -r -d 
     mv "$folder" "$RUN_DIR/$new_name"
 done
 
-## Move to N:
+echo "Transferring files to N"
+# Move to N:
 smbclient $SMB_HOST -A $SMB_AUTH -D $SMB_DIR <<EOF
 prompt OFF
 recurse ON
@@ -72,3 +79,7 @@ lcd $HOME/fastq
 mput *
 EOF
 
+## Clean up
+rm -rf fastq
+
+echo "All done!"
