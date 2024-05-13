a2df943166961b4361a3271ff9572786640434ac
jonbra <jon.brate@fhi.no>
2024-05-13 14:47:36 +0200

    Drop fastq directory for download

--- basespace.sh
+++ basespace.sh
@@ -68,7 +68,7 @@ SMB_HOST=//Pos1-fhi-svm01/styrt
 SMB_DIR=NGS/3-Sekvenseringsbiblioteker/${YEAR}/Illumina_Run
 
 # Make a fastq directory under tempdata. Download files here
-mkdir -p $BASE_DIR/fastq
+#mkdir -p $BASE_DIR/fastq
 
 echo "Getting the Run ID on the BaseSpace server"
 # List Runs on BaseSpace and get the Run id (third column separated by | and whitespaces)
@@ -76,24 +76,25 @@ id=$(/home/ngs/bin/bs list projects | grep "${RUN}" | awk -F '|' '{print $3}' | 
 
 echo "Downloading fastq files"
 # Then download the fastq files
-/home/ngs/bin/bs download project -i ${id} --extension=fastq.gz -o $BASE_DIR/fastq/${RUN}
+/home/ngs/bin/bs download project -i ${id} --extension=fastq.gz -o $BASE_DIR/${RUN}
 
 # Execute commands based on the platform specified
 if [[ $PLATFORM == "miseq" ]]; then
     echo "Running commands for MiSeq platform..."
     # Clean up the folder names
-    RUN_DIR="$BASE_DIR/fastq/${RUN}"
+    RUN_DIR="$BASE_DIR/${RUN}"
     # Find only directories in the current directory. Loop through them and rename
     # mindepth 1 excludes the RUN_DIR directory. maxdepth 1 includes only the sudirectories of RUN_DIR
     find "$RUN_DIR" -mindepth 1 -maxdepth 1 -type d -print0 | while IFS= read -r -d '' folder; do
         # Extract the name of the subdirectory
         dir_name=$(basename "$folder")
-    
+        
+        # DON'T ADD AGENS HERE. TAKE IT FROM THE FILE NAMES. 
         # Extract the sample number and add Agens name
-        new_name="${dir_name%%-*}-${AGENS}"
+#        new_name="${dir_name%%-*}-${AGENS}"
 
         # Rename the folder
-        mv "$folder" "$RUN_DIR/$new_name"
+#        mv "$folder" "$RUN_DIR/$new_name"
     done
 
     echo "Transferring files to N"
@@ -110,15 +111,14 @@ EOF
     rm -rf fastq
 elif [[ $PLATFORM == "nextseq" ]]; then
     echo "Running commands for NextSeq platform..."
-    # Define the input and output directories
-    INPUT_DIR="fastq"
+    # Define output directory
     OUTPUT_DIR=$RUN/merged
 
     # Create the output directory if it doesn't exist
     mkdir -p $BASE_DIR/$OUTPUT_DIR
 
     # Loop through each sample directory, create new subdirectories for each sample and copy the fastq files to the corresponding directory
-    for fastq in "$BASE_DIR/$INPUT_DIR"/*/*.fastq.gz; do
+    for fastq in "$BASE_DIR/$RUN"/*/*.fastq.gz; do
             # Extract the basename
             base=$(basename $fastq)
         
