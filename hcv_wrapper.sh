54230e75e9c83eca003ab2da8e3b57a418a26f66
Jon Bråte <jon.brate@fhi.no>
2024-06-06 09:40:21 +0200

    Update hcv_wrapper.sh

--- hcv_wrapper.sh
+++ hcv_wrapper.sh
@@ -108,10 +108,47 @@ docker stop gluetools-mysql
 docker rm gluetools-mysql
 #docker run --detach --name gluetools-mysql cvrbioinformatics/gluetools-mysql:latest
 
-# Install the pre-built GLUE HCV project
+# Start the gluetools-mysql container in the background
 #docker start gluetools-mysql
 docker run --detach --name gluetools-mysql cvrbioinformatics/gluetools-mysql:latest
-docker exec gluetools-mysql installGlueProject.sh ncbi_hcv_glue
+
+# Install the pre-built GLUE HCV project
+# Sometimes the docker execution fails. Retry up to 5 times
+# Set the maximum number of attempts
+max_attempts=5
+
+# Set a counter for the number of attempts
+attempt_num=1
+
+# Set a flag to indicate whether the command was successful
+success=false
+
+# Loop until the command is successful or the maximum number of attempts is reached
+while [ $success = false ] && [ $attempt_num -le $max_attempts ]; do
+  # Execute the command
+  docker exec gluetools-mysql installGlueProject.sh ncbi_hcv_glue
+
+  # Check the exit code of the command
+  if [ $? -eq 0 ]; then
+    # The command was successful
+    success=true
+  else
+    # The command was not successful
+    echo "Attempt $attempt_num failed. Trying again..."
+    # Increment the attempt counter
+    attempt_num=$(( attempt_num + 1 ))
+  fi
+done
+
+# Check if the command was successful
+if [ $success = true ]; then
+  # The command was successful
+  echo "The command was successful after $attempt_num attempts."
+else
+  # The command was not successful
+  echo "The command failed after $max_attempts attempts."
+fi
+
 
 # Make a for loop over all bam files and run HCV-GLUE
 ## Adding || true to the end of the command to prevent the pipeline from failing if the bam file is not valid
