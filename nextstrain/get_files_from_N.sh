0650b8d4cfd4185ad741106e528f62f73222d63d
Jon Bråte <jon.brate@fhi.no>
2024-06-13 12:04:32 +0200

    Update get_files_from_N.sh

--- nextstrain/get_files_from_N.sh
+++ nextstrain/get_files_from_N.sh
@@ -1,1 +1,31 @@
+#!/usr/bin/env bash
+
+# Check if the ngs_scripts directory exists, if not clone it from GitHub
+cd $HOME
+if [ -d "ngs_scripts" ]; then
+  # Make sure to pull the latest version
+  git -C ngs_scripts/ pull origin main
+else
+  git clone https://github.com/folkehelseinstituttet/ngs_scripts.git
+fi
+
+## Set up environment
+BASE_DIR=/mnt/tempdata/
+TMP_DIR=/mnt/tempdata/nextstrain
+SMB_AUTH=/home/ngs/.smbcreds
+SMB_HOST=//Pos1-fhi-svm01/styrt
+SMB_INPUT=Virologi/NGS/tmp/
+
+## Move input files from N
+
+# Create directory to hold the output of the analysis
+mkdir $TMP_DIR
+
+echo "Getting files from the N drive"
+smbclient $SMB_HOST -A $SMB_AUTH -D $SMB_INPUT <<EOF
+prompt OFF
+recurse ON
+lcd $TMP_DIR
+mget *
+EOF
 
