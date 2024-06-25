7414e81426468d4cc6d21db3c1ada8c805469a0f
Jon Bråte <jon.brate@fhi.no>
2024-06-25 11:51:29 +0200

    Update wrapper.sh

--- nextstrain/wrapper.sh
+++ nextstrain/wrapper.sh
@@ -3,21 +3,21 @@
 # Activate conda
 source ~/miniconda3/etc/profile.d/conda.sh
 
-# Check if the ngs_scripts directory exists, if not clone it from GitHub
-cd $HOME
-if [ -d "ngs_scripts" ]; then
-  # Make sure to pull the latest version
-  git -C ngs_scripts/ pull origin main
-else
-  git clone https://github.com/folkehelseinstituttet/ngs_scripts.git
-fi
-
 ## Set up environment
 BASE_DIR=/mnt/tempdata/
 TMP_DIR=/mnt/tempdata/nextstrain
 SMB_AUTH=/home/ngs/.smbcreds
 SMB_HOST=//Pos1-fhi-svm01/styrt
 SMB_DIR=Virologi/NGS/tmp/
+
+# Check if the ngs_scripts directory exists, if not clone it from GitHub
+cd $BASE_DIR
+if [ -d "ngs_scripts" ]; then
+  # Make sure to pull the latest version
+  git -C ngs_scripts/ pull origin main
+else
+  git clone https://github.com/folkehelseinstituttet/ngs_scripts.git
+fi
 
 ## Move input files from N
 
@@ -56,8 +56,8 @@ echo "Preparing Nextstrain input files from Gisaid. Takes around 30 minutes..."
 docker run --rm \
     -v $TMP_DIR/:$TMP_DIR \
     -v $HOME/ngs_scripts/nextstrain:/scripts \
-    -v $HOME/ncov/data/SC2_weekly/:$HOME/ncov/data/SC2_weekly/ \
-    -w $HOME/ncov/data/SC2_weekly/ \
+    -v $BASE_DIR/ncov/data/SC2_weekly/:$BASE_DIR/ncov/data/SC2_weekly/ \
+    -w $BASE_DIR/ncov/data/SC2_weekly/ \
     docker.io/jonbra/rsamtools:2.0 \
     Rscript /scripts/parse_Gisaid_fasta_and_metadata.R
 
@@ -67,21 +67,21 @@ docker run --rm \
     -v $TMP_DIR/:$TMP_DIR \
     -v /mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/:/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/ \
     -v $HOME/ngs_scripts/nextstrain:/scripts \
-    -v $HOME/ncov/data/SC2_weekly/:$HOME/ncov/data/SC2_weekly/ \
-    -w $HOME/ncov/data/SC2_weekly/ \
+    -v $BASE_DIR/ncov/data/SC2_weekly/:$BASE_DIR/ncov/data/SC2_weekly/ \
+    -w $BASE_DIR/ncov/data/SC2_weekly/ \
     docker.io/jonbra/rsamtools:2.0 \
     Rscript /scripts/get_data_from_BN.R
     
 # Copy nextstrain build files into the ncov directory
-cp $HOME/ngs_scripts/nextstrain/builds.yaml $HOME/ncov/my_profiles
-cp $HOME/ngs_scripts/nextstrain/colors_norwaydivisions.tsv $HOME/ncov/my_profiles
-cp $HOME/ngs_scripts/nextstrain/my_description.md $HOME/ncov/my_profiles
-cp $HOME/ngs_scripts/nextstrain/my_auspice_config.json $HOME/ncov/my_profiles
-cp $HOME/ngs_scripts/nextstrain/sites_ignored_for_tree_topology.txt $HOME/ncov/my_profiles 
+cp $HOME/ngs_scripts/nextstrain/builds.yaml $BASE_DIR/ncov/my_profiles
+cp $HOME/ngs_scripts/nextstrain/colors_norwaydivisions.tsv $BASE_DIR/ncov/my_profiles
+cp $HOME/ngs_scripts/nextstrain/my_description.md $BASE_DIR/ncov/my_profiles
+cp $HOME/ngs_scripts/nextstrain/my_auspice_config.json $BASE_DIR/ncov/my_profiles
+cp $HOME/ngs_scripts/nextstrain/sites_ignored_for_tree_topology.txt $BASE_DIR/ncov/my_profiles 
 
 conda activate nextstrain
 
-cd $HOME/ncov 
+cd $BASE_DIR/ncov 
 
 echo "Making the Nextstrain build. Should not take too long. Max 1 hour..."
 nextstrain build . --configfile my_profiles//builds.yaml --cores 14 --forceall
