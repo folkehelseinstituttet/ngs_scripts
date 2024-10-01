727465623f13a3a60420f3ed3798d327e80dea74
Jon Bråte <jon.brate@fhi.no>
2024-10-01 09:22:13 +0200

    Added nextflow pull

--- hcv_wrapper.sh
+++ hcv_wrapper.sh
@@ -99,6 +99,9 @@ docker run --rm \
 # Activate the conda environment that holds Nextflow
 conda activate NEXTFLOW
 
+# Make sure the latest pipeline is available
+nextflow pull jonbra/viralseq
+
 # Start the pipeline
 echo "Map to references and create consensus sequences"
 nextflow run jonbra/viralseq/main.nf -r master -profile server --input "$HOME/$RUN/samplesheet.csv" --outdir "$HOME/$RUN" --agens $AGENS -with-tower -bg --platform "illumina"
