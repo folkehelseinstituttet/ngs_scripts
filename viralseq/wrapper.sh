1ab8e1302f627416b42c504ffad8fcc497251143
Jon Bråte <jon.brate@fhi.no>
2024-11-04 13:52:38 +0100

    Revert to copy entire results folder and drop the tarball

--- viralseq/wrapper.sh
+++ viralseq/wrapper.sh
@@ -204,7 +204,7 @@ mv $HOME/$RUN/summarize/Genotype_mapping_summary_long_LW_import_with_glue.csv $H
 ## Then move the results to the N: drive
 echo "Moving results to the N: drive"
 mkdir $HOME/out
-tar -cf $HOME/out/$RUN.tar $HOME/$RUN/
+mv $RUN/ out/
 
 smbclient $SMB_HOST -A $SMB_AUTH -D $SMB_OUTPUT <<EOF
 prompt OFF
