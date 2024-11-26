d8e946b130bf23fdeafe96cdb1daf730049e0d9e
RasmusKoRiis <79254436+RasmusKoRiis@users.noreply.github.com>
2024-11-26 13:33:40 +0100

    Update wrapper.sh

--- nextstrain/influenza/wrapper.sh
+++ nextstrain/influenza/wrapper.sh
@@ -5,11 +5,11 @@ source ~/miniconda3/etc/profile.d/conda.sh
 
 ## Set up environment
 BASE_DIR=/mnt/tempdata/
-TMP_DIR=/mnt/tempdata/nextstrain
-OUT_DIR=/mnt/tempdata/out
+TMP_DIR=/mnt/tempdata/flu_nextstrain
+OUT_DIR=/mnt/tempdata/flu_nextstrain_out
 SMB_AUTH=/home/ngs/.smbcreds
 SMB_HOST=//Pos1-fhi-svm01/styrt
-SMB_DIR=Virologi/NGS/tmp/
+SMB_DIR=Virologi/NGS/tmp/flu_nextstrain
 
 # Check if the ngs_scripts directory exists, if not clone it from GitHub
 cd $HOME
@@ -23,15 +23,20 @@ fi
 # Check if the ncov repo exists, if not clone it from GitHub
 cd $BASE_DIR
 if [ -d "ncov" ]; then
-  cd ncov
+  cd seasonal-flu
   # Make sure to pull the latest version
   git pull origin master
   git stash
   git pull origin master
   git stash pop
 else
-  git clone https://github.com/nextstrain/ncov.git
+  git clone https://github.com/nextstrain/seasonal-flu.git
 fi
+
+## Make NIPH-profile
+cd seasonal_flu/profiles
+mkdir niph
+cd $BASE_DIR
 
 ## Move input files from N
 
@@ -46,60 +51,17 @@ lcd $TMP_DIR
 mget *
 EOF
 
-# Untar the Gisaid files
-echo "Untaring the Gisaid files. Takes around 30 minutes..."
-cd $TMP_DIR
-tar -xf metadata*.tar.xz
-rm readme.txt
-tar -xf sequences*.tar.xz
-rm readme.txt
-rm *.tar.xz
-cd $HOME
-
-# Index the Gisaid fasta file
-echo "Indexing the Gisaid fasta file. Takes an hour or so..."
-docker run --rm \
-    -v $TMP_DIR/:$TMP_DIR \
-    -v $HOME/ngs_scripts/nextstrain:/scripts \
-    -w $TMP_DIR \
-    docker.io/jonbra/rsamtools:2.0 \
-    Rscript /scripts/index_fasta.R
-
-# Parse the Gisaid files and prepare Nextstrain inputs
-echo "Preparing Nextstrain input files from Gisaid. Takes around 30 minutes..."
-docker run --rm \
-    -v $TMP_DIR/:$TMP_DIR \
-    -v $HOME/ngs_scripts/nextstrain:/scripts \
-    -v $BASE_DIR/ncov/data/SC2_weekly/:$BASE_DIR/ncov/data/SC2_weekly/ \
-    -w $BASE_DIR/ncov/data/SC2_weekly/ \
-    docker.io/jonbra/rsamtools:2.0 \
-    Rscript /scripts/parse_Gisaid_fasta_and_metadata.R
-
-# Parse BN files and prepare Nextstrain inputs
-echo "Preparing Nextstrain input files from BN. Takes around 30 minutes..."
-docker run --rm \
-    -v $TMP_DIR/:$TMP_DIR \
-    -v /mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/:/mnt/N/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/SARS-CoV-2/ \
-    -v $HOME/ngs_scripts/nextstrain:/scripts \
-    -v $BASE_DIR/ncov/data/SC2_weekly/:$BASE_DIR/ncov/data/SC2_weekly/ \
-    -w $BASE_DIR/ncov/data/SC2_weekly/ \
-    docker.io/jonbra/rsamtools:2.0 \
-    Rscript /scripts/get_data_from_BN.R
-    
 # Copy nextstrain build files into the ncov directory
-cp $HOME/ngs_scripts/nextstrain/builds.yaml $BASE_DIR/ncov/my_profiles
-cp $HOME/ngs_scripts/nextstrain/pango_list.txt $BASE_DIR/ncov/my_profiles
-cp $HOME/ngs_scripts/nextstrain/colors_norwaydivisions.tsv $BASE_DIR/ncov/my_profiles
-cp $HOME/ngs_scripts/nextstrain/my_description.md $BASE_DIR/ncov/my_profiles
-cp $HOME/ngs_scripts/nextstrain/my_auspice_config.json $BASE_DIR/ncov/my_profiles
-cp $HOME/ngs_scripts/nextstrain/sites_ignored_for_tree_topology.txt $BASE_DIR/ncov/my_profiles 
+cp $HOME/ngs_scripts/nextstrain/influenza/fhi/builds.yaml $BASE_DIR/seasonal_flu/profile/niph
+cp $HOME/ngs_scripts/nextstrain/influenza/fhi/config.yaml $BASE_DIR/seasonal_flu/profile/niph
+cp $HOME/ngs_scripts/nextstrain/influenza/fhi/prepare_data.smk $BASE_DIR/seasonal_flu/profile/niph
 
 conda activate nextstrain
 
-cd $BASE_DIR/ncov 
+cd $BASE_DIR/seasonal_flu 
 
-echo "Making the Nextstrain build. Should not take too long. Max 1 hour..."
-nextstrain build . --configfile my_profiles/builds.yaml --cores 14 --forceall
+echo "Making the Nextstrain build.
+nextstrain build .  --configfile profiles/niph/builds.yaml --cores 14 --forceall
 
 echo "Build finished. Copying auspice files to N for inspection."
 # Copy the nohup output file to the OUT_DIR folder for easier copying to N
