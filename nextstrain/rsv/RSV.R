778dada3078df77b96757c2e48bb61d1a2924fee
RasmusKoRiis <79254436+RasmusKoRiis@users.noreply.github.com>
2024-12-06 14:35:51 +0100

    Added seperation of files for RSV A and RSV B

--- nextstrain/rsv/RSV.R
+++ nextstrain/rsv/RSV.R
@@ -50,14 +50,47 @@ rsvdb <- rsvdb %>%
 # Now select the required columns
 #fludb <- fludb %>% select("key", "ngs_sekvens_resultat", "pasient_fylke_nr", "pasient_alder", "prove_tatt", "tessy_variable", "pasient_kjonn", "prove_innsender_id", "pasient_fylke_name", "prove_kategori")
 merged_df <- rsvdb %>% select("nc_id", "pasient_fylke_name", "ngs_clade", "prove_tatt", "ngs_coverage", "prove_innsender_navn", "prove_region", "prove_country", "ngs_clade", "ngs_coverage", "nc_qc_overall_score", "nc_qc_overall_status",
-                              "nc_alignment_start", "nc_alignment_end" )
+                              "nc_alignment_start", "nc_alignment_end","ngs_sekvens_resultat")
+
+merged_df <- merged_df %>% filter(ngs_coverage > 0.6)
+
+# Filter data for RSVA and RSVB
+merged_df_rsva <- merged_df %>% filter(ngs_sekvens_resultat == "RSVA")
+merged_df_rsvb <- merged_df %>% filter(ngs_sekvens_resultat == "RSVB")
 
 
 
 ################### FASTA FILE :
 
 # Assuming merged_df is your original dataframe
-tmp <- merged_df %>%
+tmp_rsva <- merged_df_rsva %>%
+  transmute(
+    accession = gsub("\\.", "_", nc_id),
+    genbank_accession_rev = "",
+    strain = nc_id,
+    date = format(as.Date(prove_tatt), "%Y-%m-%d"),
+    region = prove_region,
+    country = prove_country,
+    division = pasient_fylke_name,
+    location = "",
+    host = "Human",
+    date_submitted = "",
+    sra_accession = "",
+    abbr_authors = "",
+    authors = "",
+    institution = "",
+    clade = ngs_clade,
+    G_clade = "",
+    qc.overallScore = nc_qc_overall_score,
+    qc.overallStatus = nc_qc_overall_status,
+    alignmentScore = "",
+    alignmentStart = nc_alignment_start,
+    alignmentEnd = nc_alignment_end,
+    genome_coverage = ngs_coverage,
+    G_coverage = "0.4",
+    F_coverage = "0.4"
+  )
+tmp_rsvb <- merged_df_rsvb %>%
   transmute(
     accession = gsub("\\.", "_", nc_id),
     genbank_accession_rev = "",
@@ -116,7 +149,10 @@ desired_order <- c(
 )
 
 
-submission <- tmp %>%
+submission_rsva <- tmp_rsva %>%
+  select(all_of(desired_order))
+
+submission_rsvb <- tmp_rsvb %>%
   select(all_of(desired_order))
 
 # Function to write filtered FASTA file with nc_id as header
@@ -148,21 +184,31 @@ output_dir_csv <- "N:/Virologi/NGS/1-NGS-Analyser/1-Rutine/2-Resultater/RSV/11-N
 today_date <- format(Sys.Date(), "%Y-%m-%d")
 parent_dir <- file.path(output_dir_csv, paste0(today_date, "_Nextstrain_Build"))
 
+
 # Create the parent directory if it doesn't exist
 if (!dir.exists(parent_dir)) dir.create(parent_dir)
 
 # Define the output directories for each type
-output_dir <- file.path(parent_dir, "virus_RSV")
+output_dir_A <- file.path(parent_dir, "virus_RSV_A")
+# Define the output directories for each type
+output_dir_B <- file.path(parent_dir, "virus_RSV_B")
 
 
 # Create directories if they don't exist
-dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
+dir.create(output_dir_A, recursive = TRUE, showWarnings = FALSE)
+dir.create(output_dir_B, recursive = TRUE, showWarnings = FALSE)
+
+
 
 
 # Write the XLS files for each type
-write_xlsx(submission, file.path(output_dir, "metadata.xls"))
+write_xlsx(submission_rsva, file.path(output_dir_A, "metadata.xls"))
 # Write the TSV file for the dataframe
-write.table(submission, file.path(output_dir, "metadata.tsv"), sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+write.table(submission_rsva, file.path(output_dir_A, "metadata.tsv"), sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+# Write the XLS files for each type
+write_xlsx(submission_rsvb, file.path(output_dir_B, "metadata.xls"))
+# Write the TSV file for the dataframe
+write.table(submission_rsvb, file.path(output_dir_B, "metadata.tsv"), sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
 
 
 # Add `nc_id` to `filtered_seq` by joining with `rsvdb` on `key`
@@ -171,7 +217,7 @@ filtered_seq <- filtered_seq %>%
 
 
 # Filter `filtered_seq` based on `nc_id`
-filtered_seq <- filtered_seq %>% filter(nc_id %in% submission_ids)
+#filtered_seq <- filtered_seq %>% filter(nc_id %in% submission_id)
 filtered_seq <- filtered_seq %>% filter(experiment == "Genome")
 
 
@@ -183,18 +229,12 @@ parent_dir <- file.path(output_dir_csv, paste0(today_date, "_Nextstrain_Build"))
 # Create the parent directory if it doesn't exist
 if (!dir.exists(parent_dir)) dir.create(parent_dir)
 
-# Define the output directory
-output_dir <- file.path(parent_dir, "virus_RSV")
-
-
-# Create directories if they don't exist
-dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
-
 
 # Step 4: Define the output FASTA file 
 
-output_fasta <- file.path(output_dir, "sequences.fasta")
+output_fasta_A <- file.path(output_dir_A, "sequences.fasta")
+output_fasta_B <- file.path(output_dir_B, "sequences.fasta")
 
-write_fasta(output_fasta, filtered_seq)
-
+write_fasta(output_fasta_A, filtered_seq)
+write_fasta(output_fasta_B, filtered_seq)
 
