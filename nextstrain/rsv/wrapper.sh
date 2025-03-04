66f3f4bef0e667c0905972a41632234f7ac04cc2
RasmusKoRiis <79254436+RasmusKoRiis@users.noreply.github.com>
2025-03-04 09:05:25 +0100

    Update wrapper.sh

--- nextstrain/rsv/wrapper.sh
+++ nextstrain/rsv/wrapper.sh
@@ -71,13 +71,19 @@ cp $BASE_DIR/rsv_nextstrain/Nextstrain_Ref_database/RSVA/metadata_world.tsv.gz $
 cp $BASE_DIR/rsv_nextstrain/Nextstrain_Ref_database/RSVA/sequences_world.fasta.xz   $BASE_DIR/rsv/data/a
 DATA_DIR="$BASE_DIR/rsv/data/a"
 
-python merge_and_clean.py a
+cd $BASE_DIR/rsv/
+conda activate nextstrain
+
+python3 merge_and_clean.py a
 
 # Decompress sequences_world.fasta.xz
 unxz -f ${DATA_DIR}/sequences_world.fasta.xz
 
 # Merge local and world sequences
 cat ${DATA_DIR}/sequences.fasta ${DATA_DIR}/sequences_world.fasta > ${DATA_DIR}/combined_sequences.fasta
+
+conda deactivate 
+conda activate SEQKIT
 
 seqkit rmdup -n ${DATA_DIR}/combined_sequences.fasta -o ${DATA_DIR}/sequences.fasta
 rm ${DATA_DIR}/combined_sequences.fasta
@@ -99,7 +105,11 @@ cp $BASE_DIR/rsv_nextstrain/Nextstrain_Ref_database/RSVB/metadata_world.tsv.gz $
 cp $BASE_DIR/rsv_nextstrain/Nextstrain_Ref_database/RSVB/sequences_world.fasta.xz   $BASE_DIR/rsv/data/b
 DATA_DIR="$BASE_DIR/rsv/data/b"
 
+conda deactivate 
+conda activate nextstrain
 python merge_and_clean.py b
+conda deactivate 
+conda activate SEQKIT
 
 # Decompress sequences_world.fasta.xz
 unxz -f ${DATA_DIR}/sequences_world.fasta.xz
@@ -121,7 +131,7 @@ rm ${DATA_DIR}/metadata_world.tsv.gz
 rm ${DATA_DIR}/sequences_world.fasta
 
 
-conda activate nextstrain
+#conda activate nextstrain
 
 cd $BASE_DIR/rsv
 
