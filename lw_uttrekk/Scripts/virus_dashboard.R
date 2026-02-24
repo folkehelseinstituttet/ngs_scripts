47737671f5172e98cd13a9bf76e590cb007bca8c
Jon Bråte <jon.brate@fhi.no>
2026-02-24 11:44:23 +0100

    Started updating

--- lw_uttrekk/Scripts/virus_dashboard.R
+++ lw_uttrekk/Scripts/virus_dashboard.R
@@ -2,7 +2,7 @@ library(odbc)
 library(tidyverse)
 library(lubridate)
 
-# Version 1.1
+# Version 1.2
 
 ## ==================================================
 ## Validate required environment variables
@@ -81,7 +81,7 @@ faggrupper <- c("HEP_HIV", "HPV", "INFLUENSA", "MMR", "POL_ENT", "ROTA", "VIRUS_
 # Hente ut metadata om alle prøver fra SAMPLE-tabellen
 samples <- tbl(con, "SAMPLE_VIEW") %>% 
   # Velge ut relevant kolonner først. Dette for å forenkle hva som skal hentes ut av driveren
-  select(SAMPLE_NUMBER, TEXT_ID, GROUP_NAME, SAMPLED_DATE, RECD_DATE, X_MEDICAL_REVIEW, STATUS, X_AGENS) %>% 
+  select(ORIGINAL_SAMPLE, PARENT_SAMPLE, TEMPLATE, SAMPLE_NUMBER, TEXT_ID, GROUP_NAME, SAMPLED_DATE, RECD_DATE, X_MEDICAL_REVIEW, STATUS, X_AGENS, PATIENT) %>% 
   # Filtrer på faggrupper
   filter(GROUP_NAME %in% faggrupper) %>% 
   collect() %>% 
@@ -100,13 +100,13 @@ ngs_info <- test %>%
   # Behold virusprøver
   filter(SAMPLE_NUMBER %in% samples_sample_number) %>% 
   # Behold enten "NGS" eller "NGS_PREP"
-  filter(ANALYSIS == "NGS" | ANALYSIS == "NGS_PREP") %>%
+  filter(ANALYSIS == "NGS" | ANALYSIS == "NGS_PREP" | ANALYSIS == "SC2_NGS" | ANALYSIS == "MPX_WGS") %>%
   # Gruppere prøvene etter "A" eller andre i X_TECH_REVIEW
   mutate(bucket = case_when(
     ANALYSIS == "NGS_PREP" & X_TECH_REVIEW == "A" ~ "Auth_NGS_PREP", # First check if NGS_PREP og A
-    ANALYSIS == "NGS_PREP"                        ~ "Rej_NGS_PREP", # Deretter hvis NGS_PREP, men ikke A
+    ANALYSIS == "NGS_PREP"                        ~ "NotAuth_NGS_PREP", # Deretter hvis NGS_PREP, men ikke A
     X_TECH_REVIEW == "A"                          ~ "Auth_NGS_SEQ", # Hvis ikke ANALYSIS = NGS_PREP må det være NGS
-    TRUE                                          ~ "Rej_NGS_SEQ"  # Resten må være NGS_SEQ og ikke A
+    TRUE                                          ~ "NotAuth_NGS_SEQ"  # Resten må være NGS_SEQ og ikke A
   )) %>% 
   # Beholde relevante kolonner
   select(SAMPLE_NUMBER, bucket, BATCH) %>% 
@@ -120,7 +120,8 @@ ngs_info <- test %>%
   
   
 # Joine med sample-info
-final <- left_join(samples, ngs_info, by = c("SAMPLE_NUMBER" = "SAMPLE_NUMBER"))
+final <- left_join(samples, ngs_info, by = c("SAMPLE_NUMBER" = "SAMPLE_NUMBER")) %>%
+  rename("SAMPLE.STATUS" = STATUS)
 
 # Write final data file
 write_tsv(final, outfile)
