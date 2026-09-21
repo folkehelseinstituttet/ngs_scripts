c55c22856d6cbd1948c0e45e76594412ae2f8492
jonbra <jon.brate@fhi.no>
2026-09-21 12:05:54 +0000

    First test

---
+++ hav_sequencing/wrapper.sh
@@ -0,0 +1,17 @@
+bash ~/hav_seq/scripts/hav_wrapper.sh
+# Sette opp variabler
+# Sjekke evt. filer tilstede
+# Starte en log-fil
+# Skrive beskjeder til log-fil + skjerm
+
+# Kopiere filer fra N: (manuelf flytte metadata.tsv)
+# - metadata.tsv
+# - fasta (Sanger)
+# - Lokal database
+# Kopiere metadata.tsv fra V: (venter på tilgang)
+
+# Synce hav_seq til nyeste versjon
+#git pull -C /home/ngs/folkehelseinstituttet/hav_seq/
+# Kjøre scriptet - "sub-wrapper" fra /home/ngs/folkehelseinstituttet/hav_seq/
+
+# Flytte resultater tilbake til N:
